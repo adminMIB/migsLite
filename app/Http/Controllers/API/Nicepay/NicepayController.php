8e6ba2d2deb3b24587d571f58ab0ce07ec0f00c6
amrdevac <amrullahdev@yahoo.com>
2022-01-05 13:30:26 +0700

    menambahkan fungsi notifikasi pembayaran

--- app/Http/Controllers/API/Nicepay/NicepayController.php
+++ app/Http/Controllers/API/Nicepay/NicepayController.php
@@ -8,6 +8,8 @@ use App\Services\PaymentGateaway\Nicepay\Nicepay;
 use App\Services\Response\Nicepay\ConStoreResponse;
 use App\Http\Controllers\API\Nicepay\ConvienceStore\ConvienceStore;
 use App\Http\Controllers\API\Nicepay\VirtualAccount\VirtualAccount;
+use App\Models\PembayaranTransaksi;
+use App\Services\Response\Nicepay\VirtualAccResponse;
 
 class NicepayController
 {
@@ -15,7 +17,35 @@ class NicepayController
 
     public function register(Request $request)
     {
-        // dd(Nicepay::register($request));
-        return ConStoreResponse::get(VersioningApiService::version_1_0(), Nicepay::register($request));
+        $response = ConStoreResponse::get(VersioningApiService::version_1_0(), Nicepay::register($request));
+        return PembayaranTransaksi::create($response["api_data"]);
+    }
+
+    public function virtual_account(Request $request)
+    {
+        $response = VirtualAccResponse::get(VersioningApiService::version_1_0(), Nicepay::register($request));
+        return PembayaranTransaksi::create($response["api_data"]);
+    }
+
+
+
+
+    public function notifikasiPembayaran(Request $request)
+    {
+
+        $jumlah_dibayarkan = $request["amt"];
+        $tXid = $request["tXid"];
+        $authorisasi_notifikasi = $request["merchantToken"];
+        $invoice = $request["referenceNo"];
+        $validasi_authorisasi = hash('sha256', Nicepay::$imid . $tXid . $jumlah_dibayarkan . Nicepay::$merchant_key);
+
+        if ($validasi_authorisasi == $authorisasi_notifikasi) {
+            PembayaranTransaksi::where("invoice_pembayaran", $invoice)->update([
+                "status_pembayaran" => "TERBAYAR",
+                "waktu_terbayar" => date("Y-m-d h:i:s"),
+            ]);
+        }
+
+        return "Berhasil";
     }
 }
