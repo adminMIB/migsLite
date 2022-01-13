73fed92f88d7cdaa4a1b8603cdf0603fff17ca2b
amrdevac <amrullahdev@yahoo.com>
2022-01-13 22:54:23 +0700

    fixing apu

--- routes/api.php
+++ routes/api.php
@@ -1,44 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AlamatController;
-use App\Http\Controllers\API\JenisMemberController;
-use App\Http\Controllers\API\KostumerMemberController;
+
 use App\Http\Controllers\API\Nicepay\NicepayController;
-use App\Http\Controllers\API\PaymentMethod\PaymentMethodController;
 use App\Http\Controllers\API\PaymentService\PaymentServiceController;
 use App\Http\Controllers\API\PembayaranController;
 
 Route::resources(["payment-service" => PaymentServiceController::class]);
-Route::resources(["payment-method" => PaymentMethodController::class]);
-
-Route::post("nicepay/payment/cvs", [NicepayController::class, "register"])->name("index");
-Route::post("nicepay/payment/virtual-account", [NicepayController::class, "virtual_account"])->name("nicepay.va");
-Route::post("nicepay/payment/convenience-store", [NicepayController::class, "convenience_store"])->name("nicepay.cvs");
 
 Route::post("nicepay-notifikasi", [NicepayController::class, "notifikasiPembayaran"])->name("index");
 
 Route::resources(["pembayaran" => PembayaranController::class]);
 
 
-Route::prefix("nicepay")->name("nicepay.")->group(function () {
-    Route::get("mitra-constore", [NicepayController::class, "daftarMitraTersedia"]);
-    Route::get("bank-virtual-account", [NicepayController::class, "daftarBankTersedia"]);
-});
-
-
 Route::middleware(['auth:api', "akses"])->group(function () {
-    Route::resources(["jenis-member" => JenisMemberController::class]);
-    Route::resources(["kostumer-member" => KostumerMemberController::class]);
-
-    Route::prefix("alamat")->name("alamat.")->group(function () {
-        Route::post("/provinsi", [AlamatController::class, "provinsi"])->name("provinsi");
-        Route::post("/wilayah", [AlamatController::class, "wilayah"])->name("wilayah");
-        Route::post("/kecamatan", [AlamatController::class, "kecamatan"])->name("kecamatan");
-        Route::post("/kelurahan", [AlamatController::class, "kelurahan"])->name("kelurahan");
-        Route::post("/kodepos", [AlamatController::class, "kode_pos"])->name("kode_pos");
-        Route::get("/negara", [AlamatController::class, "negara"])->name("negara");
-    });
+    // Route::prefix("alamat")->name("alamat.")->group(function () {
+    //     Route::post("/provinsi", [AlamatController::class, "provinsi"])->name("provinsi");
+    //     Route::post("/wilayah", [AlamatController::class, "wilayah"])->name("wilayah");
+    //     Route::post("/kecamatan", [AlamatController::class, "kecamatan"])->name("kecamatan");
+    //     Route::post("/kelurahan", [AlamatController::class, "kelurahan"])->name("kelurahan");
+    //     Route::post("/kodepos", [AlamatController::class, "kode_pos"])->name("kode_pos");
+    //     Route::get("/negara", [AlamatController::class, "negara"])->name("negara");
+    // });
 });
 
 
---
+++ routes/pg/api_nicepay.php
@@ -0,0 +1,12 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Nicepay\NicepayController;
+
+
+Route::prefix("nicepay")->name("nicepay.")->group(function () {
+    Route::get("mitra-constore", [NicepayController::class, "daftarMitraTersedia"])->name("ready.cvs");
+    Route::get("bank-virtual-account", [NicepayController::class, "daftarBankTersedia"])->name("ready.va");
+    Route::post("payment/virtual-account", [NicepayController::class, "virtual_account"])->name("payment.va");
+    Route::post("payment/convenience-store", [NicepayController::class, "convenience_store"])->name("payment.vcs");
+});
