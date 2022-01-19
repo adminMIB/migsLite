f9306b3da46d3266fb38df11d5859fe621ec7bc9
amrdevac <amrullahdev@yahoo.com>
2022-01-19 16:40:58 +0700

    menambahkan api artajasa ke route service

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -49,6 +49,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/pg/api_nicepay.php'));
 
             Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/pg/api_artajasa.php'));
+
+            Route::prefix('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/default/default.php'));
 
