a9bc73a4155ed95ef17bef0c9bbfc87e0a65cd64
amrdevac <amrullahdev@yahoo.com>
2022-01-19 16:41:45 +0700

    fixing api

--- routes/api.php
+++ routes/api.php
@@ -7,21 +7,11 @@ use App\Http\Controllers\API\PaymentService\PaymentServiceController;
 use App\Http\Controllers\API\PembayaranController;
 
 Route::resources(["payment-service" => PaymentServiceController::class]);
-
 Route::post("nicepay-notifikasi", [NicepayController::class, "notifikasiPembayaran"])->name("index");
 
 Route::resources(["pembayaran" => PembayaranController::class]);
 
-
 Route::middleware(['auth:api', "akses"])->group(function () {
-    // Route::prefix("alamat")->name("alamat.")->group(function () {
-    //     Route::post("/provinsi", [AlamatController::class, "provinsi"])->name("provinsi");
-    //     Route::post("/wilayah", [AlamatController::class, "wilayah"])->name("wilayah");
-    //     Route::post("/kecamatan", [AlamatController::class, "kecamatan"])->name("kecamatan");
-    //     Route::post("/kelurahan", [AlamatController::class, "kelurahan"])->name("kelurahan");
-    //     Route::post("/kodepos", [AlamatController::class, "kode_pos"])->name("kode_pos");
-    //     Route::get("/negara", [AlamatController::class, "negara"])->name("negara");
-    // });
 });
 
 
