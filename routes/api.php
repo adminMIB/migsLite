42b051c45b7cc0f0252efa1c20dac5b94d1b442c
amrdevac <amrullahdev@yahoo.com>
2022-01-21 17:24:39 +0700

    menambahkan route api modul client

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,14 @@ Route::resources(["pembayaran" => PembayaranController::class]);
 
 // Route::prefix()
 
-Route::controller(ClientKeyController::class)->group(function () {
-    Route::get('/orders/{id}', 'show');
-    Route::post('/orders', 'store');
+Route::controller(ClientKeyController::class)->prefix("client-key")->name("client-key.")->group(function () {
+    Route::get('/', 'index')->name("index");
+    Route::post('/', 'store')->name("store");
+    Route::get('/{kd_client_key}', 'show')->name("update");
+    Route::put('/{kd_client_key}', 'update')->name("update");
+    Route::put('/activated/{kd_client_key}', 'activate')->name("activate");
+    Route::put('/deactivated/{kd_client_key}', 'deactivate')->name("deactivate");
+    Route::get('/search', 'search')->name("search");
 });
 
 Route::middleware(['auth:api', "akses"])->group(function () {
